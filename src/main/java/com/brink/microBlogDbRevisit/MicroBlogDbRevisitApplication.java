6814af758cf61877ff0ee5a73599e1a11be09c92
jeffthebrink <jeffthebrink@gmail.com>
2017-09-07 13:02:45 -0600

    initial commit

---
+++ src/main/java/com/brink/microBlogDbRevisit/MicroBlogDbRevisitApplication.java
@@ -0,0 +1,12 @@
+package com.brink.microBlogDbRevisit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBlogDbRevisitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBlogDbRevisitApplication.class, args);
+	}
+}
